fd57fbe48db14385d26dbf1b75c926074e09ce72
jrkd5 <jaroslaw.dulinski@gmail.com>
2021-01-26 20:38:33 +0100

    Postgresql configuration.

--- django3_by_example/settings.py
+++ django3_by_example/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'django3_by_example.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'password1',
     }
 }
 
