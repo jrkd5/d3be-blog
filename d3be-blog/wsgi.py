c90bd2abad3d0de2fbb55469b7202f34cb376219
jrkd5 <jaroslaw.dulinski@gmail.com>
2021-02-07 20:32:03 +0100

    Change project name.

--- d3be-blog/wsgi.py
+++ d3be-blog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django3_by_example project.
+WSGI config for d3be-blog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django3_by_example.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'd3be-blog.settings')
 
 application = get_wsgi_application()
